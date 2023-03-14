df6c2ff7b83bfaabed547bfe2b803bdcb9324a7c
lakihaomal <lhw150030@utdallas.edu>
2023-03-14 09:36:27 -0500

    general updates

--- firmware/xu4Mqtt/runAll.sh
+++ firmware/xu4Mqtt/runAll.sh
@@ -11,15 +11,15 @@ sleep 5
 python3 readI2c.py &
 sleep 5
 
-kill $(pgrep -f 'GPSReader.py')
-sleep 5
-python3 GPSReader.py &
-sleep 5
+# kill $(pgrep -f 'GPSReader.py')
+# sleep 5
+# python3 GPSReader.py &
+# sleep 5
 
-kill $(pgrep -f 'batteryReader.py')
-sleep 5
-python3 batteryReader.py &
-sleep 5
+# kill $(pgrep -f 'batteryReader.py')
+# sleep 5
+# python3 batteryReader.py &
+# sleep 5
 
 
 python3 ipReader.py
