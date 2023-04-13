64b5e30a762a79f35790e9dd307368063f8ab35e
lakihaomal <lhw150030@utdallas.edu>
2023-04-13 11:56:16 -0500

    battery reader update

--- firmware/xu4Mqtt/batteryReader.py
+++ firmware/xu4Mqtt/batteryReader.py
@@ -1,5 +1,5 @@
 import datetime
-import odroid_wiringpi as wpi
+from subprocess import run
 import time
 from collections import OrderedDict
 from mintsXU4 import mintsSensorReader as mSR
@@ -9,37 +9,44 @@ import os
 debug  = False 
 
 def getPiSugarOutput(command,ignoreStr):
-    
-
-
+    print(command)
+    print(ignoreStr)
+    data = run("echo " +command  " | nc -q 1 127.0.0.1 8423",capture_output=True,shell=True)
+    print(data.stdout)
+    print(data.stderr)
 
 def main():
-    while True:
-        try:
-            dateTime          = datetime.datetime.now()
-            batteryLevelRaw   = wpi.analogRead(25)
-            referenceLevelRaw = wpi.analogRead(29)
-            batteryLevel      = batteryLevelRaw*(2.1*2)/(4095)
-            #batteryLevelPercetage = batteryLevel*(100/4.2)
-            batteryLevelPercetage = (batteryLevel-2.5)*(100)/(3.25-2.5)
-
-
-            sensorDictionary =  OrderedDict([
-                    ("dateTime"               ,str(dateTime)), # always the same
-                    ("batteryLevelRaw"        ,str(batteryLevelRaw)), # check with arduino code
-                    ("batteryLevel"           ,str(batteryLevel)),
-                    ("batteryLevelPercetage"  ,str(batteryLevelPercetage)),
-                    ("referenceLevelRaw"      ,str(referenceLevelRaw))
-                    ])
-            
-            if batteryLevelPercetage< 5:
-                # Shut Down Node 
-                print("Low Battery - Shutting Down PC")
-#                os.system("sudo shutdown now")
     
 
-            mSR.sensorFinisher(dateTime,"MWBL002",sensorDictionary)
-            time.sleep(30)
+    while True:
+        try:
+            getPiSugarOutput("get battery","ignoreStr")
+            time.sleep(10)
+
+#             dateTime          = datetime.datetime.now()
+#             batteryLevelRaw   = wpi.analogRead(25)
+#             referenceLevelRaw = wpi.analogRead(29)
+#             batteryLevel      = batteryLevelRaw*(2.1*2)/(4095)
+#             #batteryLevelPercetage = batteryLevel*(100/4.2)
+#             batteryLevelPercetage = (batteryLevel-2.5)*(100)/(3.25-2.5)
+
+
+#             sensorDictionary =  OrderedDict([
+#                     ("dateTime"               ,str(dateTime)), # always the same
+#                     ("batteryLevelRaw"        ,str(batteryLevelRaw)), # check with arduino code
+#                     ("batteryLevel"           ,str(batteryLevel)),
+#                     ("batteryLevelPercetage"  ,str(batteryLevelPercetage)),
+#                     ("referenceLevelRaw"      ,str(referenceLevelRaw))
+#                     ])
+            
+#             if batteryLevelPercetage< 5:
+#                 # Shut Down Node 
+#                 print("Low Battery - Shutting Down PC")
+# #                os.system("sudo shutdown now")
+    
+
+            # mSR.sensorFinisher(dateTime,"MWBL002",sensorDictionary)
+            # time.sleep(30)
 
 
         except Exception as e:
