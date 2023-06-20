9ff29b8ac0208d97f071fc1682203f26645b8bf4
lakihaomal <lhw150030@utdallas.edu>
2023-06-20 02:36:25 -0500

    wrist band update

--- firmware/xu4Mqtt/wristBandReader.py
+++ firmware/xu4Mqtt/wristBandReader.py
@@ -5,10 +5,10 @@
 
 # while True:
 #     print(bluetooth.get_adv())
-
+import struct
 
 import time
-
+from collections import OrderedDict
 # Protocol defined here:
 #     https://github.com/zh2x/BCI_Protocol
 # Thanks as well to:
@@ -24,7 +24,7 @@ from adafruit_ble.services.standard.device_info import DeviceInfoService
 from adafruit_ble.services.nordic import UARTService 
 uart = UARTService()
 uart_connection = None
-
+from mintsXU4 import mintsSensorReader as mSR
 
 # from adafruit_ble_berrymed_pulse_oximeter import BerryMedPulseOximeterService
 
@@ -65,12 +65,43 @@ while True:
             print(wrist_band_connection)
             uart_service = wrist_band_connection[UARTService]
             while wrist_band_connection.connected:    
-                one_byte = uart_service.read(4)
-                if one_byte:
-                    print(datetime.datetime.now())
-                    print(one_byte)
-                    print()
-
+                byteArrayReceived = uart_service.read(4)
+                if byteArrayReceived:
+                    dateTime = datetime.datetime.now()
+                    sensorIDIndex    = struct.unpack('<B',bytes.fromhex(\
+                                            byteArrayReceived.hex()[0:2]))[0]
+                    parametorIDIndex = struct.unpack('<B',bytes.fromhex(\
+                                            byteArrayReceived.hex()[2:4]))[0]
+                    if sensorIDIndex == 61: 
+                        if parametorIDIndex  == 1:
+                            hrAmplitude =  struct.unpack('<H',bytes.fromhex(\
+                                            byteArrayReceived.hex()[4:8]))[0]
+                            sensorDictionary =  OrderedDict([
+                                    ("dateTime"    ,str(dateTime)),
+                                    ("hrAmplitude" ,str(hrAmplitude))
+                            ])
+                            print(sensorDictionary)
+                            mSR.sensorFinisher(dateTime,"PSA109",sensorDictionary)
+                    
+                    if sensorIDIndex == 27: 
+                        if parametorIDIndex  == 1:
+                            if byteArrayReceived.hex()[4:8]!= "ffff":
+                                hexArray    = byteArrayReceived.hex()
+                                hexArray    = hexArray[:6] + "0" + hexArray[6 + 1:]
+                                int32HexStr  = "0000" + hexArray[6:8]+ hexArray[4:6]
+                                int32In  = struct.unpack('>I',bytes.fromhex(\
+                                            int32HexStr))[0]
+                                temperature = int32In/16.0
+                                int1 = int.from_bytes(bytearray.fromhex(int32HexStr), byteorder='big')
+                                int2 = int.from_bytes(bytearray([0,1,0,0]) , byteorder='big')
+                                if int1  & int2:
+                                     temperature = temperature-256
+                                sensorDictionary =  OrderedDict([
+                                    ("dateTime"    ,str(dateTime)),
+                                    ("temperature" ,str(temperature))
+                                ])
+                                print(sensorDictionary)
+                                mSR.sensorFinisher(dateTime,"MCP9808",sensorDictionary)
     except connection_error:
         try:
             pulse_ox_connection.disconnect()
