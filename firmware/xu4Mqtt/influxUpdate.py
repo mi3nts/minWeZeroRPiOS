c0fcd095bf583db4251059f32a2bb10f0e1e7226
lakihaomal <lhw150030@utdallas.edu>
2024-08-16 15:13:36 -0500

    influx updates

--- firmware/xu4Mqtt/influxUpdate.py
+++ firmware/xu4Mqtt/influxUpdate.py
@@ -161,19 +161,21 @@ def sendCSV2Influx(csvFile,nodeID,sensorID,nodeName):
             reader            = csv.DictReader((line.replace('\0','') for line in f) )
             rowList           = list(reader)
             for rowData in rowList:
-                dateTimeRow = datetime.strptime(rowData['dateTime'],'%Y-%m-%d %H:%M:%S.%f')
-                point = Point(sensorID)  # Replace with your measurement name
-                point.tag("device_id", nodeID)
-                point.tag("device_name", nodeName)
-                point.time(dateTimeRow, WritePrecision.NS) 
-                # print(point)
-                # Dynamically add fields based on the headers
-                for header in reader.fieldnames:
-                    if header not in tag_columns and header != time_column:
-                        point.field(header, isFloat(rowData[header]))
-                # print(point)
-                # sequence.append(point)
-
+                try:
+                    dateTimeRow = datetime.strptime(rowData['dateTime'],'%Y-%m-%d %H:%M:%S.%f')
+                    point = Point(sensorID)  # Replace with your measurement name
+                    point.tag("device_id", nodeID)
+                    point.tag("device_name", nodeName)
+                    point.time(dateTimeRow, WritePrecision.NS) 
+                    # print(point)
+                    # Dynamically add fields based on the headers
+                    for header in reader.fieldnames:
+                        if header not in tag_columns and header != time_column:
+                            point.field(header, isFloat(rowData[header]))
+                    # print(point)
+                    # sequence.append(point)
+                except ValueError as e:
+                    print(f"An error occurred: {e}")
 
 
         # with InfluxDBClient(url=influxURL, token=influxToken, org=influxOrg) as client:
