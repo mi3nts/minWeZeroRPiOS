fe326e6eb26d425dbbc687ef6f07fe36c98ceeda
lakihaomal <lhw150030@utdallas.edu>
2024-08-20 11:12:28 -0500

    influx updates

--- firmware/xu4Mqtt/influxUpdate.py
+++ firmware/xu4Mqtt/influxUpdate.py
@@ -217,10 +217,11 @@ def sendCSV2InfluxToday(csvFile,nodeID,sensorID,nodeName,fileDate):
                     dateTimeRow = datetime.strptime(rowData['dateTime'], '%Y-%m-%d %H:%M:%S.%f')
                     if sensorID == "BME280" or sensorID == "BME680":
                         print(rowData)
-                        if rowData["humidity"] > 0:
-                            rowData["dewPoint"] = 243.04 * (math.log(rowData["humidity"] / 100.0) + ((17.625 * rowData["temperature"]) / (243.04 + rowData["temperature"]))) / (17.625 - math.log(rowData["humidity"] / 100.0) - ((17.625 * rowData["temperature"]) / (243.04 + rowData["temperature"])))
+                        if float(rowData["humidity"]) > 0:
+                            rowData["dewPoint"] = str(243.04 * (math.log(float(rowData["humidity"]) / 100.0) + ((17.625 * float(rowData["temperature"])) / (243.04 + float(rowData["temperature"])))) \
+                                / (17.625 - math.log(float(rowData["humidity"]) / 100.0) - ((17.625 * float(rowData["temperature"])) / (243.04 + float(rowData["temperature"])))))
                         else:
-                            rowData["dewPoint"] = 0.0
+                            rowData["dewPoint"] = str(0)
                         print(rowData)
                     # print(lastDateTime)
                     # print(dateTimeRow)
